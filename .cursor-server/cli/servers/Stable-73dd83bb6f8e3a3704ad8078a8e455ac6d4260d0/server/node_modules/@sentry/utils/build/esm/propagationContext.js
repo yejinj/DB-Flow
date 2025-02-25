13f395b04a0c3de18b40e96a64db629887ecbc57
yejinj <sitting077@gmail.com>
2025-02-25 13:46:12 +0900

    Initial commit

---
+++ .cursor-server/cli/servers/Stable-73dd83bb6f8e3a3704ad8078a8e455ac6d4260d0/server/node_modules/@sentry/utils/build/esm/propagationContext.js
@@ -0,0 +1,13 @@
+import { uuid4 } from './misc.js';
+
+/**
+ * Returns a new minimal propagation context
+ */
+function generatePropagationContext() {
+  return {
+    traceId: uuid4(),
+    spanId: uuid4().substring(16),
+  };
+}
+
+export { generatePropagationContext };
