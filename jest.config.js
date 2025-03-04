82404061d5014bfba7e90719e9f362f95eeebae7
yejinj <sitting077@gmail.com>
2025-03-04 16:34:52 +0900

    fix: test file fail case debugged

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: [
+    '.cursor-server',
+    '.vscode-server',
+    '.nvm'
+  ],
+  testMatch: [
+    "**/tests/**/*.test.js"
+  ]
+};
