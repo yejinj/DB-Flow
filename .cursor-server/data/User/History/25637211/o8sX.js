033ddf7c83b86f6baf965c6341270b29b117998c
yejinj <sitting077@gmail.com>
2025-03-05 13:59:57 +0900

    chore: remove files specified in .gitignore

--- .cursor-server/data/User/History/25637211/o8sX.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    modulePathIgnorePatterns: [
-      "<rootDir>/.cursor-server/",
-      "<rootDir>/.vscode-server/",
-      "<rootDir>/.nvm/"
-    ]
-  };
-  
