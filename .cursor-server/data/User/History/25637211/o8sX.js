f72aa0b64d9b44f851c10e1d713fe11a94601a1b
yejinj <sitting077@gmail.com>
2025-03-05 13:26:15 +0900

    chore: work arrangement

---
+++ .cursor-server/data/User/History/25637211/o8sX.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnvironment: 'node',
+    modulePathIgnorePatterns: [
+      "<rootDir>/.cursor-server/",
+      "<rootDir>/.vscode-server/",
+      "<rootDir>/.nvm/"
+    ]
+  };
+  
