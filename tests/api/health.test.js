b8dad0ebe5c75e7ae1ca9a58e5157958f0686fa1
yejinj <sitting077@gmail.com>
2025-03-13 14:11:17 +0900

    updated: develop branch updated Thu 13 Mar 2025 02:11:17 PM KST

--- tests/api/health.test.js
+++ tests/api/health.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
-const app = require('../../server');
+const app = require('../../app');
+
+jest.setTimeout(20000);
 
 describe('API Health Check', () => {
+  beforeAll(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  });
+
   it('should return MongoDB connection status', async () => {
     const res = await request(app).get('/health');
     expect(res.statusCode).toBe(200);
