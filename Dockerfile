4cd4034bf00845db4b62722e9887510eaf0b0d51
yejinj <sitting077@gmail.com>
2025-03-19 13:45:28 +0900

    chore: main branch updated with dev branch

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
-FROM node:18
+FROM node:20
 
-WORKDIR /usr/src/app
-
-COPY package*.json ./
-
-RUN npm install
-
+WORKDIR /app
 COPY . .
-
-EXPOSE 3000
+RUN npm install
 
 CMD ["npm", "start"]
---
+++ config/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: '@shelf/jest-mongodb',
+    testEnvironment: 'node',
+    modulePathIgnorePatterns: [
+      '.cursor-server',
+      '.vscode-server',
+      '.nvm'
+    ],
+    testMatch: [
+      "**/tests/**/*.test.js"
+    ]
+  };
+  
