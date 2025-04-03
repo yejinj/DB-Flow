f5ca07ae65a2975a1ecd1b7ffbdcefafc9acf551
yejinj <sitting077@gmail.com>
2025-04-03 15:18:09 +0900

    chore: add inline comments

--- tests/mongodb/readPreference.test.js
+++ tests/mongodb/readPreference.test.js
@@ -5,7 +5,7 @@ test('Read from secondary using readPreference', async () => {
   const client = new MongoClient(config.uri, {
     readPreference: 'secondaryPreferred',
     replicaSet: 'rs0',
-    ...config.defaultOptions
+    ...config.defaultOptions // 공통 옵션 병합
   });
 
   try {
@@ -14,7 +14,7 @@ test('Read from secondary using readPreference', async () => {
     const db = client.db('test');
     const collection = db.collection('readPrefTest');
 
-    await collection.insertOne({ name: 'replica-read-check' });
+    await collection.insertOne({ name: 'replica-read-check' }); // 테스트용 문서 삽입
     const result = await collection.findOne({ name: 'replica-read-check' });
 
     expect(result.name).toBe('replica-read-check');
