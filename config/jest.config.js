2c50cf989d6b136f44d7f27ec559da1db8af712b
yejinj <sitting077@gmail.com>
2025-04-03 10:18:22 +0900

    refactor: user scehma modified

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    preset: '@shelf/jest-mongodb',
+    preset: '@shelf/jest-mongodb', // 몽고DB 테스트 프리셋, 인메모리 환경 이용
     testEnvironment: 'node',
     modulePathIgnorePatterns: [
       '.cursor-server',
@@ -10,4 +10,3 @@ module.exports = {
       "**/tests/**/*.test.js"
     ]
   };
-  
